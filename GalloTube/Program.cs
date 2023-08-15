32e2bc1adfa3f56d26871eb451c1aabff9eeb1c4
Joany Camily da Silva <joanycamily23@gmail.com>
2023-08-15 12:28:53 -0300

    linha 466

---
+++ GalloTube/Program.cs
@@ -0,0 +1,48 @@
+using GalloTube.Data;
+using GalloTube.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Objetos auxiliares de conexão
+string conn = builder.Configuration.GetConnectionString("GalloTube");
+var version = ServerVersion.AutoDetect(conn);
+
+// Serviço de conexão com banco de dados
+builder.Services.AddDbContext<AppDbContext>( options =>
+    options.UseMySql(conn, version)
+);
+
+// Serviço de gestão de usuário - identity
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
